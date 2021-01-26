13dbf433b89dec229008375c09d4a10b6bb6bbc6
Saia Meafoou <saiameafoou@Saias-MacBook-Air.local>
2021-01-26 17:12:44 +1100

    example of a shooter firing guns

--- src/main/java/com/meafoou/saia/chapter/one/Foo.java
+++ src/main/java/com/meafoou/saia/chapter/one/Foo.java
@@ -1,4 +1,52 @@
 package com.meafoou.saia.chapter.one;
 
 public class Foo {
+    public static void main(String[] args) {
+        String joinedString = String.join("-", args);
+//        int result1 = add(4, 33);
+//        int result2 = add(4, 99);
+//        int finalResult = add(result1, result2);
+//        System.out.println("RESULT = " + finalResult);
+
+        AK47 ak47 = new AK47();
+        Glock glock = new Glock();
+
+        Shooter.takeGun(ak47).pullTrigger();
+        Shooter.takeGun(glock).pullTrigger();
+    }
+
+    public static class Shooter {
+        private Gun gun;
+        public Shooter(Gun gun) {
+            this.gun = gun;
+        }
+
+        public static Shooter takeGun(Gun gun) {
+            return new Shooter(gun);
+        }
+
+        public void pullTrigger() {
+            System.out.println(gun.fire());
+        }
+    }
+
+    public static int add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public interface Gun {
+        String fire();
+    }
+
+    public static class AK47 implements Gun {
+        public String fire() {
+            return "ak-bang-bang";
+        }
+    }
+
+    public static class Glock implements Gun {
+        public String fire() {
+            return "glock-bang";
+        }
+    }
 }
